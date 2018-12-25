5e10b8eed885b0475b70af33ab3a9fe473364fc8
Tiffany Yeh <tiffany.yeh@umbocv.com>
2018-12-25 16:56:16 -0500

    added delete to courselist completed inbox fragment next: edit function

--- module_cache_data/src/main/java/com/chenhuiyeh/module_cache_data/AppDatabase.java
+++ module_cache_data/src/main/java/com/chenhuiyeh/module_cache_data/AppDatabase.java
@@ -3,7 +3,9 @@ package com.chenhuiyeh.module_cache_data;
 import android.content.Context;
 
 import com.chenhuiyeh.module_cache_data.dao.CoursesDao;
+import com.chenhuiyeh.module_cache_data.dao.InboxDao;
 import com.chenhuiyeh.module_cache_data.model.CourseInfo;
+import com.chenhuiyeh.module_cache_data.model.InboxItem;
 import com.chenhuiyeh.module_cache_data.utils.TimeTableTypeConverters;
 
 import androidx.room.Database;
@@ -11,12 +13,13 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
 
-@Database(entities = {CourseInfo.class}, version = 1)
+@Database(entities = {CourseInfo.class, InboxItem.class}, version = 2)
 @TypeConverters(TimeTableTypeConverters.class)
 public abstract class AppDatabase extends RoomDatabase {
 
     // new daos can be added here
     public abstract CoursesDao courseDao();
+    public abstract InboxDao inboxDao();
 
     private static final String DATABASE_NAME = "timetable_db";
 
--- module_cache_data/src/main/java/com/chenhuiyeh/module_cache_data/dao/CoursesDao.java
+++ module_cache_data/src/main/java/com/chenhuiyeh/module_cache_data/dao/CoursesDao.java
@@ -10,7 +10,6 @@ import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
-import androidx.room.TypeConverter;
 import androidx.room.TypeConverters;
 import androidx.room.Update;
 
