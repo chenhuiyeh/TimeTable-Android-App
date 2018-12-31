9c2ccb3835f58766e2ae94f6df67a7550e2a3e55
Tiffany Yeh <tiffany.yeh@umbocv.com>
2018-12-30 20:36:40 -0500

    add comments

--- app/src/main/java/com/chenhuiyeh/timetable/ui/TimeTableUI/CourseBlock.java
+++ app/src/main/java/com/chenhuiyeh/timetable/ui/TimeTableUI/CourseBlock.java
@@ -11,7 +11,23 @@ import com.chenhuiyeh.timetable.R;
 
 import androidx.appcompat.widget.AppCompatTextView;
 import androidx.core.content.ContextCompat;
+/** Copyright (c) 2018 Wei Chen Yao (https://yaoandy107.github.io/).
 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License. **/
+
+/**
+ * Created by blackmaple on 2017/5/9.
+ */
 public class CourseBlock extends AppCompatTextView {
     private int row;
     private int col;
--- app/src/main/java/com/chenhuiyeh/timetable/ui/TimeTableUI/CourseTableLayout.java
+++ app/src/main/java/com/chenhuiyeh/timetable/ui/TimeTableUI/CourseTableLayout.java
@@ -1,5 +1,4 @@
 package com.chenhuiyeh.timetable.ui.TimeTableUI;
-
 import android.content.Context;
 import android.os.Build;
 import android.os.Handler;
@@ -24,6 +23,24 @@ import java.util.List;
 import java.util.Locale;
 
 import androidx.annotation.Nullable;
+
+/** Copyright (c) 2018 Wei Chen Yao (https://yaoandy107.github.io/).
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License. **/
+
+/**
+ * Created by blackmaple on 2017/5/9.
+ */
 
 public class CourseTableLayout extends LinearLayout {
     private static final String TAG = "CourseTableLayout";
